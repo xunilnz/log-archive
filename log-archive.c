99cf741e5c99b5a3d0238786223e5a8066ecd4d4
zl4kj <kim@xunil.co.nz>
2025-04-30 18:15:32 +1200

    Added target directory argument for archive destination

--- log-archive.c
+++ log-archive.c
@@ -12,18 +12,21 @@ int main(int argc,char* argv[])
 	printf("log-archive v1.0.1 by ZL4KJ\n\n");
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
-	DIR *d;
+	DIR *d, *d1;
 	struct dirent *dir;
-	if(argc==2)
+	if(argc==3)
 	{
 		char* path = argv[1];
 		char* path2 = argv[2];
+        if (!path2) path2= "./";
 		d = opendir(path);
-		if (d) 
+	    d1 = opendir(path2);	
+    if (d && d1) 
 		{
 			closedir(d);
-			printf("Archiving directory %s\n\n",path);
-			snprintf(buffer, sizeof(buffer), "tar -cf logs_archive_%d%02d%02d_%02d%02d%02d.tar.gz %s/", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, path);
+            closedir(d1);
+			printf("Archiving directory %s to %s\n\n",path, path2);
+			snprintf(buffer, sizeof(buffer), "tar -cf %slogs_archive_%d%02d%02d_%02d%02d%02d.tar.gz %s", path2, tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, path);
 			system(buffer);
 		}
 		else if (ENOENT == errno)
@@ -34,7 +37,7 @@ int main(int argc,char* argv[])
 		}
 		else
 		{
-			printf("error: no directory passed\n\nPlease use the following syntax:\n\nlog-archive </directory/to/archive>\n\n");
+			printf("error: no directory passed\n\nPlease use the following syntax:\n\nlog-archive </directory/to/archive> <target/directory>\n\nIf no target ddirectory is specified, the program will use the current.\n");
 			return 1;
 		}
 }
